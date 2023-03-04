d29eb210cd5af8684fb74b8abf5ec6c3bf98c2f5
Ivan <118657938+johndenisov@users.noreply.github.com>
2023-03-04 21:33:15 +0300

    start Python

---
+++ Lesson1.py
@@ -0,0 +1,9 @@
+username = input("Ввведите имя: ")
+if username == "Маша":
+    print("Ура, это же Маша!")
+elif username == "Марина":
+    print("Я так вас ждал, Марина!")
+elif username == "Ильнар":
+    print("Ильнар - топ")
+else:
+    print("Привет, ", username)
