c53942768ddc721a697d8045dcb9ace17b2b95c7
Ivan <118657938+johndenisov@users.noreply.github.com>
2023-03-11 18:38:19 +0300

    task4  lesson1

--- Lesson1/Task2.py
+++ Lesson1/Task2.py
@@ -1,6 +1,6 @@
-line = " "
-for i in range(5):
-    line =""
+line = "7"
+for i in range(7):
+    line = ""
     for j in range(5):
-        line += "*"
+        line += " * "
 print(line)
---
+++ Lesson1/Task3.py
@@ -0,0 +1,7 @@
+#import math
+n = int(input("Количество км за день"))
+m = int(input("Количество км маршрута"))
+print(m//n)
+#print((m+n-1)//n)
+#print(math.ceil(m//n))
+
